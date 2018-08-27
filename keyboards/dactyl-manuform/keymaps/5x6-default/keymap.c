79a958e33009329aa5b930d1fc196788f298e3dc
Kyle Dyroff <kyle.dyroff@gmail.com>
2018-08-27 09:10:43 -0400

    Update keymap to match ergodox

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,    KC_A,    KC_S,    KC_D,   KC_F,   KC_G,
    KC_LSHIFT, KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,
                        KC_TAB,  KC_BSLS,
-                               LT(_FN, KC_DEL), KC_LSFT, 
+                               LT(_FN, KC_DEL), KC_BSPC, 
                                KC_SPC,   KC_LCTRL,
                                KC_ENTER, KC_LALT,
         // right hand
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
                      KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,
                                       KC_LBRC, KC_RBRC,
-        KC_SPC, LT(_FN, KC_BSPC), 
+        KC_SPC, LT(_FN, KC_ENTER), 
         CTL_T(KC_PGUP), KC_PGDN,
         KC_LALT, KC_LGUI),
 [_FN] = KEYMAP_5x6(
